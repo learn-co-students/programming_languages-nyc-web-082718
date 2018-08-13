9655f7b51e0bbf412bcc13a4282f319c4a805751
tomlev816 <thomaslever508@gmail.com>
2018-08-13 14:56:29 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+  hash = {}
+  #binding.pry
+  languages.each do |style, data|
+    data.each do |attribute, value|
+      if hash[attribute].nil?
+        hash[attribute] = {type: value[:type],style:[style]}
+      else
+        hash[attribute][:style] << style
+        #need to check spelling 'styles' isnt a variable.  
+      end
+    end
+  end
+  return hash
 end
