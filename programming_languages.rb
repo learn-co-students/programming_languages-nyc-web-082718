847205a337882b50ce9055cf382523fb6e5aadee
github username <github email address>
2018-08-22 17:03:33 -0400

    practice iterating over and modifying nested hashes

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,15 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |category, language_type|
+    language_type.each do |language, data|
+      data.each do |key, value|
+        if new_hash.key?(language) == true
+          new_hash[language][:style] << category
+        else
+          new_hash[language] = {key => value, :style => [category]}
+        end
+      end
+    end
+  end
+  new_hash
 end
