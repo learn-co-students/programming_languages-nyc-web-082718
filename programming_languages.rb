edf549abf9ec6e8e19652a8a37b5460ecfa5f84a
Chris Bojemski <chris.bojemski@gmail.com>
2018-08-23 12:33:20 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,19 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  ref_langs = {}
+  languages.each do |style, langs|
+    langs.each do |lang, types|
+      if ref_langs.has_key?(lang)
+        #add style to styles array 
+        ref_langs[lang][:style] << style 
+      else 
+        ref_langs[lang] = {
+          :type => types[:type],
+          :style => [style]
+        }
+      end
+    end 
+  end 
+  ref_langs 
 end
